5d9f5f804add6e7a6647023ad8a7a9556d0025f6
nxxnnaa <i21022@hb.dhbw-stuttgart.de>
2024-04-03 16:07:15 +0200

    different changes before auth

--- src/components/CrimeProfile.vue
+++ src/components/CrimeProfile.vue
@@ -1,20 +1,24 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-title>Title</ion-title>
+      <ion-title>{{props.markerData[0].title}}</ion-title>
       <ion-buttons slot="end">
-        <ion-button @click="closeModal()">Close</ion-button>
+        <ion-button @click="closeModal()"><ion-icon name="close-outline"></ion-icon></ion-button>
       </ion-buttons>
     </ion-toolbar>
   </ion-header>
   <ion-content class="ion-padding">
     <p>{{props.markerData[0].title}}</p>
+    <p>{{props.markerData[0].summary}}</p>
+    <p>{{props.markerData[0].status}}</p>  
   </ion-content>
 </template>
 
 <script setup lang="ts">
 import { AllCases } from "@/types/supabase-global";
 import { IonContent, IonHeader, IonTitle, modalController } from "@ionic/vue";
+
+
 
 // PROPS
 const props = defineProps<{
--- src/views/CaseProfileView.vue
+++ src/views/CaseProfileView.vue
@@ -1,48 +1,46 @@
 <template>
-    <ion-page>
-      <ion-header :translucent="true">
-        <ion-toolbar>
-          <ion-title>Case Profile</ion-title>
-        </ion-toolbar>
-      </ion-header>
-  
-      <ion-content :fullscreen="true">
-        
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton } from '@ionic/vue';
-  </script>
-  
-  <style scoped>
-  #container {
-    text-align: center;
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Case Profile</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-list>
+        <ion-item v-for="(item, index) in dbMarkers" :key="index">
+          <ion-list >
+            <ion-item v-for="(itemInArray, index2) in item" :key="index2">
+              <ion-label>
+                <h2>{{ itemInArray.title }}</h2>
+                <p>{{ itemInArray.summary }}</p>
+                <!-- Weitere Daten anzeigen, falls erforderlich -->
+              </ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-item>
+      </ion-list>
     
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { mapService } from "../services/map-service";
+
+const dbMarkers = ref([]);
+
+onMounted(async () => {
+  try {
+    const dbMarkers = await mapService.getAllCases();
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  
-  #container strong {
-    font-size: 20px;
-    line-height: 26px;
-  }
-  
-  #container p {
-    font-size: 16px;
-    line-height: 22px;
-    
-    color: #8c8c8c;
-    
-    margin: 0;
-  }
-  
-  #container a {
-    text-decoration: none;
-  }
-  </style>
-  
+});
+</script>
+
+<style scoped>
+/* Stil für die gerenderte Liste hier hinzufügen */
+</style>
